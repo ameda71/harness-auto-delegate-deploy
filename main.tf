f210ad23e58a1e467782e7e84736ece8e9275e15
ameda71 <amedasaiteja75@gmail.com>
2025-05-06 15:25:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
-
   deletion_protection = false
-
-  initial_node_count = var.node_count
+  initial_node_count  = var.node_count
 
   node_config {
     machine_type = var.node_machine_type
