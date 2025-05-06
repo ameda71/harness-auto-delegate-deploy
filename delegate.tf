e8fb85c6412b3ed644be43d40b0edc32db02f79f
ameda71 <amedasaiteja75@gmail.com>
2025-05-06 14:57:55 +0530

    Update delegate.tf

--- delegate.tf
+++ delegate.tf
@@ -29,7 +29,7 @@ module "delegate" {
 
   account_id = "ucHySz2jQKKWQweZdXyCog"
   delegate_token = "NTRhYTY0Mjg3NThkNjBiNjMzNzhjOGQyNjEwOTQyZjY="
-  delegate_name = "terraform-delegate-saiteja"
+  delegate_name = "terraform-delegate-teja"
   deploy_mode = "KUBERNETES"
   namespace = "harness-delegate-ng"
   manager_endpoint = "https://app.harness.io"
@@ -37,5 +37,4 @@ module "delegate" {
   replicas = 1
   upgrader_enabled = true
 }
-
 
